843a7c1f35396479965174698544d8369c9668bb
emilie <emilie.le-lan@epitech.eu>
2024-03-24 13:51:16 +0100

    add route for access to details album page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,10 @@ import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import GenreDetails from './components/Genres/GenreDetails/GenreDetails';
 import GenresList from './components/Genres/GenreList/GenresList';
+import AlbumList from './components/Album/AlbumList/AlbumList';
+import AlbumDetails from './components/Album/AlbumDetails/AlbumDetails';
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/genres" exact component={GenresList} />
+          <Route path="/genres/:genreId" exact component={GenreDetails} />
+          <Route path="/albums" exact component={AlbumList} />
+          <Route path="/albums/:id" exact component={AlbumDetails} /> 
         </Switch>
       </div>
     </div>
